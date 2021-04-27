914f85a85db43c3b80e8e833508c0bd82652cf74
SulaimanAlRafee <83151670+SulaimanAlRafee@users.noreply.github.com>
2021-04-27 06:20:41 +0300

    Create input and output.py

---
+++ input and output.py
@@ -0,0 +1,38 @@
+#find out the area of a triangle
+base = float(input("Please enter base:"))
+height = float(input("Please enter height:"))
+area = 0.5 * height * base
+print("The area of the triangle is ", area)
+
+#print the average of two numbers
+# num1 = float(input("Please enter 1st number:"))
+# num2 = float(input("Please enter 2nd number:"))
+# average = (num1 + num2) / 2
+# print("The average is", average)
+
+#1 convert from inch to cm, cm = 2.54inch
+# inch = float(input("Please enter length in inches: "))
+# cm = inch * 2.54
+# #print("the length is cm centimeters") #WRONG
+# print("the length is",cm,"centimeters")
+
+#escape characters
+#print("1\t\'2\'\t3\t\"4\"") #print quotes
+#print("1\t2\t3\t4") #print tabs using \t
+#print("1\n2\n3\n4") #print new lines using \n
+
+#Adding two numbers, and conversion to int
+# num1 = 4.3
+# num2 = 1.6
+# sum = num1+num2
+# print("Sum of two numbers = ",sum)
+
+# sum_new = int(sum)
+# print("Sum of two numbers after conversion to int = ",sum_new)
+
+# student_name = input("What is your name?")
+# student_YOB = int(input("What year you were born in?"))
+# print("Your name is:",student_name,"and you were born in ",student_YOB)
+
+# age = 2021 - student_YOB
+# print("You are",age,"Years old")
